0e8c681e477850918e8e40b674ef2e7c8facc59b
nithia <nithia341@gmail.com>
2024-09-10 19:08:05 -0600

    guardar

---
+++ src/componentes/boton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../hojas-de-estilo/boton.css"
+
+function Boton ({ texto,botonClic,manejarClic }){
+    return (
+        <button className={ botonClic ? "boton-clic": "boton-reiniciar" }
+        onClick={manejarClic} > 
+        {texto}
+        </button>
+    );
+}
+
+export default Boton;
